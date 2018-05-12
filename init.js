2d62959a7f57d45bcc174717f2a2ae4bd6e34889
senttor <oksentiuk.viktor@gmail.com>
2018-05-12 22:56:07 +0300

    fix2
     клас ImprovedFighter

--- init.js
+++ init.js
@@ -22,3 +22,24 @@ class Fighter {
     }
 
 }
+
+//=====================================
+
+class ImprovedFighter extends Fighter {
+
+    doubleHit (enemy, point) {
+        let doubleHitpoint = point * 2;
+        super.hit(enemy, doubleHitpoint);
+    }
+
+}
+
+//===================================
+user1 =  ['Mike', 11, 110];
+
+user2 = ['Jonh',12, 90];
+
+
+var  fighter = new Fighter(...user1);
+
+var improvedFighter = new ImprovedFighter(...user2);
